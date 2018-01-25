eb7fe385aed40ab303ef0912ed48e5f1f56eff78
yiyi <yiyi@windows10.microdone.cn>
2018-01-25 16:50:23 +0800

    修改拦截范围
    Change-Id: If8eea921a41367e833647f48edfc06dbefaf4a5d
    Signed-off-by: yiyi <yiyi@windows10.microdone.cn>

--- src/main/java/cn/itcast/springboot/demo/HelloApplication.java
+++ src/main/java/cn/itcast/springboot/demo/HelloApplication.java
@@ -1,11 +1,14 @@
 package cn.itcast.springboot.demo;
 
 import java.nio.charset.Charset;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -13,15 +16,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.itcast.springboot.javaconfig.SpringConfig;
+import cn.itcast.springboot.javaconfig.User;
+import cn.itcast.springboot.javaconfig.UserService;
+
 @Controller
 @SpringBootApplication(exclude = { RedisAutoConfiguration.class })
 @Configuration
 public class HelloApplication {
 
-    @RequestMapping("hello")
+
+    @RequestMapping("index")
     @ResponseBody
     public String hello() {
-        return "hello world！ 传智播客！";
+        return "泰岳金服！";
+    }
+
+    @RequestMapping("user/list")
+    @ResponseBody
+    public List<User> userList() {
+        // 通过Java配置来实例化Spring容器
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        
+        // 在Spring容器中获取Bean对象
+        UserService userService = context.getBean(UserService.class);
+        context.destroy();
+        return userService.queryUserList();
     }
 
     @Bean
